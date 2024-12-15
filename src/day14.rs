088ed124d7dc5139e4f2e8b77fadabf0befeff0d
Galen Elias <gelias@gmail.com>
2024-12-14 20:43:37 -0800

    Switch Day14 part 2 to use RLE minimization as the strategy

--- src/day14.rs
+++ src/day14.rs
@@ -34,6 +34,9 @@ pub fn solve(inputs: Vec<String>) {
 	const MID_X: i64 = WIDTH / 2;
 	const MID_Y: i64 = HEIGHT / 2;
 
+	let mut min_rle_size = (WIDTH * HEIGHT) as usize;
+	let mut min_rle_size_second = 0;
+
 	for second in 0.. {
 		for robot in robots.iter_mut() {
 			robot.pos.0 += robot.vel.0;
@@ -58,10 +61,21 @@ pub fn solve(inputs: Vec<String>) {
 			set
 		});
 
-        // For some reason part 2 seems to correspond to every robot being in a unique position
-        // That is dumb...
-		if positions.len() == robots.len() {
-			println!("------Part 2. Second {}------", second + 1);
+		let mut robots_str = String::with_capacity((WIDTH * HEIGHT) as usize);
+		for y in 0..HEIGHT {
+			for x in 0..WIDTH {
+				if positions.contains(&(x, y)) {
+					robots_str.push('#');
+				} else {
+					robots_str.push('.');
+				}
+			}
+		}
+
+		// Get run length encoded size of robots_str
+		let rle_size = robots_str.chars().chunk_by(|&c| c).into_iter().count();
+
+		if rle_size == min_rle_size && second > 10000 {
 			for y in 0..HEIGHT {
 				for x in 0..WIDTH {
 					if positions.contains(&(x, y)) {
@@ -72,8 +86,14 @@ pub fn solve(inputs: Vec<String>) {
 				}
 				println!();
 			}
-			println!();
+			println!("Part 2: {} ({})", min_rle_size_second, min_rle_size);
 			break;
+		}
+
+		if rle_size < min_rle_size {
+			min_rle_size = rle_size;
+			min_rle_size_second = second + 1;
+			println!("New minimum: {} at {}", min_rle_size, min_rle_size_second);
 		}
 
 		if second == 99 {
